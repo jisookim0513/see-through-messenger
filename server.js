9fc47c7879747f86585c02d1c24b3728cf2f304e
Pierre Karashchuk <krchtchk@gmail.com>
2015-07-05 14:56:11 -0700

    working ui (very basic)

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
-
+var randomstring = require("randomstring");
 var oneDay = 86400000;
 
 app.use(compression());
-app.use(express.static(__dirname + '/static', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/static'));
 
 app.set('port', process.env.PORT || 3000);  
 var server = app.listen(app.get('port'), function() {  
@@ -16,29 +16,28 @@ var server = app.listen(app.get('port'), function() {
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({server: server});
 
-var global_id = 0;
 
+// id -> socket
 var sockets = {};
+// cookie -> name mapping
 var cookies = {jisoo: "jisoo", pierre: "pierre"};
-var names = {jisoo: new Set([1]), pierre: new Set([2])};
+// name -> sockets for that person
+var names = {jisoo: new Set(), pierre: new Set()};
+// socket id -> name
 var socket_names = {1: "jisoo", 2: "pierre"};
+
+var global_id = 0;
 
 wss.on('connection', function(ws) {
     global_id += 1;
-
     var id = global_id;
-    var name = socket_names[id];
     console.log(id);
-    
-    ws.on('message', function(message) {
-        console.log('id %d: %s', id, message);
-        var data = JSON.parse(message);
 
-        // var name = cookies[data['cookie']];
-        // if(!name) {
-        //     return;
-        // }
+    // var name = socket_names[id];
+    var name = undefined;
 
+
+    function handle_text(data) {
         var to = undefined;
         if(name == 'jisoo') {
             to = 'pierre';
@@ -46,12 +45,15 @@ wss.on('connection', function(ws) {
             to = 'jisoo';
         }
 
-        if(!names[to] && names[to].size == 0) {
+        if(!names[to] || names[to].size == 0) {
             return;
         }
 
 
-        var msg = {from: name, to: to, text: data['text']};
+        var msg = {from: name, text: data['text']};
+        if(data['message']) {
+            msg['message'] = true;
+        }
         msg = JSON.stringify(msg);
         
         for (var tid of names[to]) {
@@ -59,11 +61,48 @@ wss.on('connection', function(ws) {
                 sockets[tid].send(msg);
             }
         }
+
+        if(data['message']) {
+            console.log('hello');
+            for (var tid of names[name]) {
+                if(sockets[tid]) {
+                    sockets[tid].send(msg);
+                }
+            }
+        }
+        
+    }
+
+    function handle_cookie(data) {
+        var c = data['cookie'];
+        if(cookies[c]) {
+            name = cookies[c];
+            names[name].add(id);
+        }
+    }
+    
+    ws.on('message', function(message) {
+        console.log('id %d: %s', id, message);
+
+        try {
+            var data = JSON.parse(message);
+        } catch (e) {
+            console.log('%s was badly formatted', message);
+            return;
+        }
+            
+        if(typeof data['text'] === 'string') {
+            handle_text(data);
+        } else if(data['cookie']) {
+            handle_cookie(data);
+        }
     });
 
     ws.on('close', function close() {
         sockets[id] = undefined;
-        names[socket_names[id]].delete(id);
+        if(names[socket_names[id]]) {
+            names[socket_names[id]].delete(id);
+        }
         console.log('disconnected');
     });
 
